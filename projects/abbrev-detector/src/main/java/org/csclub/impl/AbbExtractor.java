f1620c4a5ae6feff95fb620f97302f8f03d8d26a
Ann Voevodskaia <vaa239@gmail.com>
2013-03-17 22:10:02 +0400

    Added AbbExtractor.class Signed-off-by: Ann Voevodskaia <vaa239@gmail.com>

--- projects/abbrev-detector/src/main/java/org/csclub/impl/AbbExtractor.java
+++ projects/abbrev-detector/src/main/java/org/csclub/impl/AbbExtractor.java
@@ -12,26 +12,24 @@ import java.util.regex.Pattern;
 /**
  * Created by IntelliJ IDEA.
  * User: Ann Voevodskaia
- * Date: 17.03.13
- * Time: 16:48
  */
 public class AbbExtractor implements AbbreviationExtractor {
 
     public List<String> extract(final String sentence) {
-        Pattern p = Pattern.compile("(.*)([\\.,&!<>)(»«²]+)(.)");
+        Pattern p = Pattern.compile("(.*)([\\.,&!<>)(»«²]+)(.)");  //kills structures as "yes)."
         Matcher m;
         StringTokenizer st = new StringTokenizer(sentence, " \t\n\r ");
         List<String> wordsWithDots = new ArrayList<String>();
         String currentWord;
         currentWord = st.nextToken();
         m = p.matcher(currentWord);
-        if (currentWord.endsWith(".") && (!currentWord.equals(".")) && (!currentWord.endsWith("...")) && (!m.matches())) {
+        if (currentWord.endsWith(".") && (!currentWord.equals("."))  && (!m.matches())) {
             wordsWithDots.add(currentWord);
         }
         if (st.hasMoreElements()) currentWord = st.nextToken();
         while (st.hasMoreElements()) {
             m = p.matcher(currentWord);
-            if ((currentWord.endsWith(".")) && (!currentWord.equals(".")) && (!currentWord.endsWith("..")) && (!m.matches())) {
+            if ((currentWord.endsWith(".")) && (!currentWord.equals(".")) && (!m.matches())) {
                 wordsWithDots.add(currentWord);
             }
             currentWord = st.nextToken();
