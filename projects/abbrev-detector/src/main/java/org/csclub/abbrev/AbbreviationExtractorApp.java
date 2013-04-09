16b7b2cc2d51f1217e8805aee23056bb1af0bac4
afaf <amosov.f@mail.ru>
2013-04-09 12:04:41 +0400

    Frequency algorithm analisys

--- projects/abbrev-detector/src/main/java/org/csclub/abbrev/AbbreviationExtractorApp.java
+++ projects/abbrev-detector/src/main/java/org/csclub/abbrev/AbbreviationExtractorApp.java
@@ -6,11 +6,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
-
-import org.csclub.abbrev.impl.AbbExtractor;
-import org.csclub.abbrev.impl.AbbreviationCounter_impl;
+import org.csclub.abbrev.evaluation.AbbreviationEvaluator;
+import org.csclub.abbrev.evaluation.ConfusionMatrix;
 import org.csclub.abbrev.impl.AbbreviationExtractor_impl;
+import org.csclub.abbrev.impl.Delimiter;
 import org.csclub.abbrev.impl.TrieAbbreviationCounter;
 
 /**
@@ -40,13 +41,54 @@ public class AbbreviationExtractorApp  {
         abbrevCounter.print(System.out);
     }
     
+    public void collate(String fileName, String encoding) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        List<Abbreviation> goldAbbreviations = new Serializer().fromTextFile(fileName, encoding);
+        
+        abbrevCounter.onNewAbbreviations(goldAbbreviations);
+        abbrevCounter.corpusProcessComplete();
+        
+        Delimiter.setShareOfAbbreviations(goldAbbreviations);
+        
+        List<Abbreviation> trueAbbreviations = new ArrayList();
+        
+        for (int i = 0; i < goldAbbreviations.size(); ++i) {
+            Abbreviation cur = goldAbbreviations.get(i);
+            if (i <= Delimiter.shareOfAbbreviations * goldAbbreviations.size()) {
+                System.out.println(i + 1 + "\tOK\t" + cur.toString(-1));
+            }
+            else {
+                System.out.println(i + 1 + "\tNOT\t" + cur.toString(-1));
+            }
+            
+            if (goldAbbreviations.get(i).getAbbrevState() == Abbreviation.AbbrevState.True) {
+                trueAbbreviations.add(goldAbbreviations.get(i));
+            }
+        }
+        System.out.println();
+        
+        AbbreviationEvaluator evaluator = new AbbreviationEvaluator(trueAbbreviations);
+        ConfusionMatrix matrix = evaluator.evaluate(Delimiter.probablyAbbreviations(goldAbbreviations));
+        
+        System.out.println("Precision:\t" + matrix.getPrecision());
+        System.out.println("Recall:\t\t" + matrix.getRecall());
+        System.out.println("F1 Measure:\t" + matrix.getF1Measure());
+    }
+    
     public static void main( String[] args ) {
-        final String path = String.format("%s/../../datasets/opencorpora/opencorpora.sent.train.ru", System.getProperty("user.dir"));
+        
+//        String path = String.format("%s/../../datasets/opencorpora/opencorpora.sent.train.ru", System.getProperty("user.dir"));
+//        try {
+//            AbbreviationExtractorApp extractor = new AbbreviationExtractorApp();
+//            extractor.extract(path, "UTF-8");
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+        
+        String path = String.format("%s/../../resources/abbreviations/abbrev-gold.txt", System.getProperty("user.dir"));
         try {
-            AbbreviationExtractorApp extractor = new AbbreviationExtractorApp ();
-            extractor.extract(path, "UTF-8");
+            AbbreviationExtractorApp extractor = new AbbreviationExtractorApp();
+            extractor.collate(path, "UTF-8");
         } catch(Exception e) {
-            e.printStackTrace();
         }
     }
 }
