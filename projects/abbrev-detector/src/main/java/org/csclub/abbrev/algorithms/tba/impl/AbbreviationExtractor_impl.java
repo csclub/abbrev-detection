29aa6e35d1b7c5e0317272b87fd89c1f6e378105
afaf <amosov.f@mail.ru>
2013-04-22 22:38:29 +0400

    From arrays to collections
    Changed String[] to List<String>

--- projects/abbrev-detector/src/main/java/org/csclub/abbrev/algorithms/tba/impl/AbbreviationExtractor_impl.java
+++ projects/abbrev-detector/src/main/java/org/csclub/abbrev/algorithms/tba/impl/AbbreviationExtractor_impl.java
@@ -1,17 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.csclub.abbrev.algorithms.tba.impl;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.csclub.abbrev.AbbreviationUtils;
 import org.csclub.abbrev.Abbreviation;
-import org.csclub.abbrev.algorithms.tba.AbbreviationExtractor;
+import org.csclub.abbrev.AbbreviationUtils;
 import org.csclub.abbrev.Sentence;
+import org.csclub.abbrev.algorithms.tba.AbbreviationExtractor;
 
 /**
  *
@@ -24,7 +20,6 @@ public class AbbreviationExtractor_impl implements AbbreviationExtractor {
     
     private  Pattern simpleAbbrevPattern;
     
-    
     public AbbreviationExtractor_impl() {
         simpleAbbrevPattern = Pattern.compile("(\\p{L}|[.-])*\\p{L}+[.]$");
     }
@@ -36,26 +31,27 @@ public class AbbreviationExtractor_impl implements AbbreviationExtractor {
      */
     @Override
     public List<Abbreviation> extract(Sentence sentence) {
-        String[] tokens;
+        List<String> tokens;
         if (null != sentence.getTokens()) {
-            tokens = sentence.getTokens().toArray(new String [sentence.getTokens().size()]);
+            tokens = sentence.getTokens();
         } else {
             tokens = AbbreviationUtils.tokenize(sentence.getSentence());
         }
-        List<Abbreviation> abbreviations = new ArrayList<>();
-        for (int i = 0; i < tokens.length - 1; i++) {
-            if (tokens[i].endsWith(".")) {
+        
+        List<Abbreviation> abbreviations = new ArrayList();
+        for (int i = 0; i < tokens.size() - 1; i++) {
+            if (tokens.get(i).endsWith(".")) {
                 // get abbreviation
-                String abbrevText = getTokenAbbreviation(tokens[i]);
+                String abbrevText = getTokenAbbreviation(tokens.get(i));
                 if (null == abbrevText) {
                     continue;
                 }
-                Abbreviation abbrev = new Abbreviation(tokens[i]);
+                Abbreviation abbrev = new Abbreviation(tokens.get(i));
                 // get abbreviation context
-                if (i == 0 && tokens.length >= 1) {
-                    abbrev.addAbbrevContext(String.format("%s %s", tokens[i], tokens[1]));
-                } else if (i >= 1 && tokens.length >= 2) {
-                    abbrev.addAbbrevContext(String.format("%s %s %s", tokens[i - 1], tokens[i], tokens[i + 1]));
+                if (i == 0 && tokens.size() >= 1) {
+                    abbrev.addAbbrevContext(String.format("%s %s", tokens.get(i), tokens.get(1)));
+                } else if (i >= 1 && tokens.size() >= 2) {
+                    abbrev.addAbbrevContext(String.format("%s %s %s", tokens.get(i - 1), tokens.get(i), tokens.get(i + 1)));
                 } 
                     
                 abbreviations.add(abbrev);
