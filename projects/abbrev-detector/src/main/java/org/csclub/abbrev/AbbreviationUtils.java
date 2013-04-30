bd3aa73ccf74d100d1bef62a6798cab3159f07e5
afaf <amosov.f@mail.ru>
2013-04-30 12:33:04 +0400

    Corpus tokenizer appeared

--- projects/abbrev-detector/src/main/java/org/csclub/abbrev/AbbreviationUtils.java
+++ projects/abbrev-detector/src/main/java/org/csclub/abbrev/AbbreviationUtils.java
@@ -22,6 +22,24 @@ public class AbbreviationUtils {
         return tokens;
     }
     
+    public static List<String> tokenize(Corpus corpus) {
+        List<String> tokens = new ArrayList();
+        for (Sentence sentence : corpus.getSentences()) {
+            for (String token : sentence.getTokens()) {
+                token = token.replaceAll("\\.+", ".");
+                if (token.endsWith(".")) {
+                    if (token.length() > 1) {
+                        tokens.add(token.substring(0, token.length() - 1));
+                    }
+                    tokens.add(".");
+                } else {
+                    tokens.add(token);
+                }
+            }
+        }
+        return tokens;
+    }
+    
     public static Object createClassInstance(String className) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         @SuppressWarnings("unchecked")
         Class<? extends Object> klass = (Class<? extends Object>)Class.forName(className);
