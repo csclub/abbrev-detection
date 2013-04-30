4fc51a25ce4753f6e4fcdd24db29eac16d8d54e4
afaf <amosov.f@mail.ru>
2013-04-30 12:32:04 +0400

    Analysis of pirson chi-square test added

--- projects/abbrev-detector/src/main/java/org/csclub/abbrev/AbbreviationExtractorApp.java
+++ projects/abbrev-detector/src/main/java/org/csclub/abbrev/AbbreviationExtractorApp.java
@@ -1,14 +1,9 @@
 package org.csclub.abbrev;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import org.csclub.abbrev.algorithms.Algorithm;
 import org.csclub.abbrev.algorithms.tba.CorpusAbbreviation;
-import org.csclub.abbrev.algorithms.tba.impl.Delimiter;
 import org.csclub.abbrev.connectors.CorpusReader;
 import org.csclub.abbrev.evaluation.AbbreviationEvaluator;
 import org.csclub.abbrev.evaluation.ConfusionMatrix;
@@ -61,76 +56,6 @@ public class AbbreviationExtractorApp  extends Component {
         
         // run final algorithm steps
         algorithm.run(corpus);
-    }
-    
-    public ConfusionMatrix delimiterAnalysis(final List<Abbreviation> goldAbbreviations) {
-        return null;
-        /*
-        Delimiter.setShareOfAbbreviations(goldAbbreviations);
-        
-        List<Abbreviation> trueAbbreviations = new ArrayList();
-        
-        for (int i = 0; i < goldAbbreviations.size(); ++i) {
-            Abbreviation cur = goldAbbreviations.get(i);
-            if (i <= Delimiter.shareOfAbbreviations * goldAbbreviations.size()) {
-                System.out.println(i + 1 + "\tOK\t" + cur.toString(-1));
-                
-            }
-            else {
-                System.out.println(i + 1 + "\tNOT\t" + cur.toString(-1));
-            }
-            
-            if (goldAbbreviations.get(i).getAbbrevState() == Abbreviation.AbbrevState.True) {
-                trueAbbreviations.add(goldAbbreviations.get(i));
-            }
-          
-        }
-        System.out.println();
-        
-        AbbreviationEvaluator evaluator = new AbbreviationEvaluator(trueAbbreviations);
-        return evaluator.evaluate(Delimiter.probablyAbbreviations(goldAbbreviations));
-        * */
-    }
-    
-    public ConfusionMatrix constLimitAnalysis(final List<Abbreviation> goldAbbreviations, int lim) {
-        return null;
-        /*
-        List<Abbreviation> trueAbbreviations = new ArrayList();
-        List<Abbreviation> positiveAbbreviations = new ArrayList();
-        
-        
-        for (int i = 0; i < goldAbbreviations.size(); ++i) {
-            Abbreviation cur = goldAbbreviations.get(i);
-            if (cur.getAbbrevCount() >= lim) {
-                System.out.println(i + 1 + "\tOK\t" + cur.toString(-1));
-                positiveAbbreviations.add(cur);
-            }
-            else {
-                System.out.println(i + 1 + "\tNOT\t" + cur.toString(-1));
-            }
-            
-            if (goldAbbreviations.get(i).getAbbrevState() == Abbreviation.AbbrevState.True) {
-                trueAbbreviations.add(goldAbbreviations.get(i));
-            }
-        }
-        System.out.println();
-        
-        AbbreviationEvaluator evaluator = new AbbreviationEvaluator(trueAbbreviations);
-        return evaluator.evaluate(positiveAbbreviations);
-        * */
-    }
-    
-    public void collate(String fileName, String encoding) throws FileNotFoundException, UnsupportedEncodingException, IOException {
-       /* 
-        List<Abbreviation> goldAbbreviations = new Serializer().fromTextFile(fileName, encoding);
-        
-        abbrevCounter.onNewAbbreviations(goldAbbreviations);
-        abbrevCounter.corpusProcessComplete();
-        
-        delimiterAnalysis(goldAbbreviations).print("Delimiter algorithm:");
-        constLimitAnalysis(goldAbbreviations, 2).print("Const limit algorithm:");
-        constLimitAnalysis(goldAbbreviations, 0).print("Zero limit algorithm:");
-        */
     }
     
     public static void main( String[] args ) {
@@ -203,6 +128,24 @@ public class AbbreviationExtractorApp  extends Component {
                 
                 ConfusionMatrix matrix = evaluator.evaluate(app.algorithm.getAbbreviations());
                 System.out.println("T-Test based: " + matrix);
+            }
+            
+            {
+                String corpusFile = Paths.get(System.getProperty("user.dir"), "../../datasets/opencorpora/opencorpora.sent.train.ru").toString();
+                Configuration config = new Configuration(new String [] 
+                                                {   
+                                                    "ConnectorClass", "org.csclub.abbrev.connectors.SentPerLineCorpusReader",
+                                                    "Connector.FileName", corpusFile,
+                                                    "Connector.FileEncoding", "UTF-8",
+                                                    "AlgorithmClass", "org.csclub.abbrev.algorithms.tba.ChiSquareTestBasedAlgorithm",
+                                                } 
+                                             );
+                AbbreviationExtractorApp app = new AbbreviationExtractorApp();
+                app.init(config);
+                app.run();
+                
+                ConfusionMatrix matrix = evaluator.evaluate(app.algorithm.getAbbreviations());
+                System.out.println("Chi sqare test based: " + matrix);
             }
             
         } catch(Exception e) {
